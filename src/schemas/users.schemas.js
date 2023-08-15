227157c3b0ba554803afb3f52f9d68422053ba82
juliana <julianapereira.dev@gmail.com>
2023-08-15 12:41:50 -0300

    FIx: Removi confirmar senha porque não precisa

--- src/schemas/users.schemas.js
+++ src/schemas/users.schemas.js
@@ -3,7 +3,6 @@ import joi from "joi";
 export const signupSchema = joi.object({
   email: joi.string().email().trim().required(),
   password: joi.string().trim().required(),
-  confirmPassword: joi.string().valid(joi.ref('password')).trim().required(),
   username: joi.string().trim().required(),
   pictureUrl: joi.string().uri().trim().required()
   });
