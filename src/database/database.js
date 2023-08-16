7d904155f64bc7b6092f73e7725dd477f705dd23
juliana <julianapereira.dev@gmail.com>
2023-08-16 20:38:21 -0300

    Mudei para testar avaliador

--- src/database/database.js
+++ src/database/database.js
@@ -8,7 +8,7 @@ const configDatabase = {
   connectionString: process.env.DATABASE_URL,
 };
 
-// if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
+if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
 
 
 export const db = new Pool(configDatabase);
