65847e09bce9a5ffe6c665f2d81e8dc34300fff7
nilteras <niltonrnascimento@hotmail.com>
2023-08-16 11:51:16 -0300

    feat: verify/insert or delete like

--- src/repositories/timeline.repository.js
+++ src/repositories/timeline.repository.js
@@ -21,3 +21,28 @@ export async function getPosts(limit = 20) {
     const result = await db.query(query, [limit]);
     return result.rows;
 }
+
+export async function isLiked(userId,postId){
+  return db.query(`
+    SELECT *
+    FROM likes
+    WHERE likes."postId" = $1
+    AND likes."userId" = $2
+  `, [postId, userId])
+}
+
+export async function insertLike(userId,postId){
+  return db.query(`
+    INSERT INTO likes
+    ("userId", "postId")
+    VALUES ($1, $2)
+  `, [userId, postId])
+}
+
+export async function deleteLike(userId, postId){
+  return db.query(`
+    DELETE FROM likes
+    WHERE "userId" = $1
+    AND "postId" = $2 
+  `, [userId, postId])
+}
--- src/routes/timeline.routes.js
+++ src/routes/timeline.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { validationSchema } from "../middlewares/validationSchema.js";
 import { publishSchema } from "../schemas/publish.schema.js";
-import { publishLink } from "../controllers/timeline.controllers.js";
+import { like, publishLink } from "../controllers/timeline.controllers.js";
 import { validationAuth } from "../middlewares/validationAuth.js";
 import { getAllPosts } from "../controllers/timeline.controllers.js";
 
@@ -9,5 +9,7 @@ const timelineRouter = Router();
 
 timelineRouter.post("/timeline", validationAuth, validationSchema(publishSchema), publishLink);
 timelineRouter.get("/timeline", validationAuth, getAllPosts);
+timelineRouter.post("/like", like)
+
 
 export default timelineRouter;
