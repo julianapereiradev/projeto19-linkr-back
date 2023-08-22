834a6bb36f39cb03079fa94f3d2550683a31b654
nilteras <niltonrnascimento@hotmail.com>
2023-08-22 20:28:48 -0300

    feat: Get and Post comments By id

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userRouter from "./users.routes.js";
 import timelineRouter from "./timeline.routes.js";
+import commentsRouter from "./comment.routes.js";
 
 const router = Router();
 
 router.use(userRouter);
 router.use(timelineRouter);
+router.use(commentsRouter);
 
 export default router;
